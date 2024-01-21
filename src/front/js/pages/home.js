e44622a6d1d504d13db7bd52afedd06a3b1962d7
Luis Fernando Roldan Ortiz <108189366+Luis-Roldan@users.noreply.github.com>
2024-01-21 20:02:15 +0000

    landingpage v1 sin estilos

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,23 +8,97 @@ export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div>
-      <h1>This is the landingpage</h1>
+    <div className="container">
+      <div className="insideHero">
+        <div>
+          <h2>Descubre la carrera que mas se ajuste a ti</h2>
+          <p>
+            Busca y encuentra carreras al rededor del mundo y encuentra la
+            perfecta para ti. Además de encontrar información adicional de la
+            carrera y reseñas de las personas que ya la han realizado.{" "}
+          </p>
+        </div>
+        <div className="filters">
+          <span>¿Dónde te gustaría correr?</span>
+          <span>Fechas</span>
+          <span>Elige tu distancia</span>
+          <button>Buscar</button>
+        </div>
+      </div>
+      <div>
+        <h2>Disfruta la carrera de tus sueños</h2>
+        <div id="carouselExampleCaptions" class="carousel slide">
+          <div class="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="0"
+              class="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            ></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="..." class="d-block w-100" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>
+                  Some representative placeholder content for the first slide.
+                </p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="..." class="d-block w-100" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>
+                  Some representative placeholder content for the second slide.
+                </p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="..." class="d-block w-100" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>
+                  Some representative placeholder content for the third slide.
+                </p>
+              </div>
+            </div>
+          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
     </div>
-    // <div className="text-center mt-5">
-    // 	<h1>Hello Rigo!!</h1>
-    // 	<p>
-    // 		<img src={rigoImageUrl} />
-    // 	</p>
-    // 	<div className="alert alert-info">
-    // 		{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-    // 	</div>
-    // 	<p>
-    // 		This boilerplate comes with lots of documentation:{" "}
-    // 		<a href="https://start.4geeksacademy.com/starters/react-flask">
-    // 			Read documentation
-    // 		</a>
-    // 	</p>
-    // </div>
   );
 };
