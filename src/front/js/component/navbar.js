afb264d72349db0d5055baafbbe7ec1fb30c814d
Luis Fernando Roldan Ortiz <108189366+Luis-Roldan@users.noreply.github.com>
2024-01-21 18:16:05 +0000

    avance 1

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,24 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import runestLogo.png from "//src/front/img/runestLogo.png";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-					<div>
-						<Link to="/signUp">
-							Sign Up
-						</Link>
-					</div>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <div>
+      <nav class="navbar bg-body-tertiary">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img src={runestLogo.png} width="30" height="24" />
+          </a>
+        </div>
+      </nav>
+    </div>
+
+    // <nav className="navbar navbar-light bg-light">
+    // 	<div className="container">
+    // 		<Link to="/">
+    // 			<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+    // 		</Link>
+    // 		<div className="ml-auto">
+    // 			<Link to="/demo">
+    // 				<button className="btn btn-primary">Check the Context in action</button>
+    // 			</Link>
+    // 			<div>
+    // 				<Link to="/signUp">
+    // 					Sign Up
+    // 				</Link>
+    // 			</div>
+    // 		</div>
+    // 	</div>
+    // </nav>
+  );
 };
