423e63a890f4b364ef8ac2482cf3d1ac1096b45a
jant7070 <50752064+jant7070@users.noreply.github.com>
2024-01-21 00:49:05 +0000

    Funcionalidad: Crear usuario Se creo el modelo user Se creo la ruta /user Funcionalidad de registro de usuario completada

--- src/api/models.py
+++ src/api/models.py
@@ -5,8 +5,9 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    hashed_password = db.Column(db.String(80), unique=False, nullable=False)
+    salt = db.Column(db.String(250), unique=False, nullable=False)
+    nombre = db.Column(db.String(200), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +16,5 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "nombre": self.nombre
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,8 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from bcrypt import gensalt
+from flask_bcrypt import check_password_hash, generate_password_hash
 
 api = Blueprint('api', __name__)
 
@@ -12,11 +14,52 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/user', methods=['POST'])
+def handle_user():
+    #obtener data de la solicitud
+    data = request.json
+    email = data.get("email")
+    password = data.get("password")
+    nombre = data.get("nombre")
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    # verificar que la data este completa
+    data_check = [email, password, nombre]
+    if None in data_check:
+        return jsonify({
+            "msg": "Faltan datos, por favor verifica tu solicitud"
+        }), 400
 
-    return jsonify(response_body), 200
+    #verificar que el correo es unico
+    usuario = User.query.filter_by(email=email).one_or_none()
+    if usuario:
+        return jsonify({
+            "msg": "Ya existe un usuario asociado a este correo"
+        }), 400
+    
+    #generar el salt y el hashed_password
+    salt = str(gensalt(), encoding="utf-8")
+    hashed_password = str(generate_password_hash(password + salt), encoding="utf-8")
+
+
+    #crear el usuario y mandarlo a la base de datos
+    nuevo_usuario = User(
+        email = email,
+        hashed_password = hashed_password,
+        salt = salt,
+        nombre = nombre
+    )
+
+    #guardar en la base de datos 
+    try:
+        db.session.add(nuevo_usuario)
+        db.session.commit()
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({
+            "msg": "Ha ocurrido un error con la base de datos"
+        }), 500
+    
+    return jsonify({
+        "salt": salt,
+        "password": hashed_password
+    }), 201
