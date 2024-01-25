38dc0348e9d105200afa61da29c2dddae604905b
Luis Fernando Roldan Ortiz <108189366+Luis-Roldan@users.noreply.github.com>
2024-01-25 01:33:01 +0000

    agregado el dropdown en el navbar pero falta los estilos

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -28,7 +28,33 @@ export const Navbar = () => {
             <span>Contacto</span>
           </Link>
         </div>
-        <div>
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Iniciar sesión
+          </button>
+          <ul class="dropdown-menu">
+            <li><Link to="/logIn" className="btn btnNav">
+              para usuarios
+            </Link></li>
+            <li><Link to="/logIn" className="btn btnNav">
+              para organizadores
+            </Link></li>
+          </ul>
+        </div>
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Registrarse
+          </button>
+          <ul class="dropdown-menu">
+            <li><Link to="/signUp" className="btn btnNav">
+              para usuarios
+            </Link></li>
+            <li><Link to="/signUpForOrganizers" className="btn btnNav">
+              para organizadores
+            </Link></li>
+          </ul>
+        </div>
+        {/* <div>
           <Link to="/logIn" className="btn btnNav">
             iniciar sesión
           </Link>
@@ -37,7 +63,7 @@ export const Navbar = () => {
           <Link to="/signUp" className="btn btnNav">
             Registrarse
           </Link>
-        </div>
+        </div> */}
         <div>
           <Link to="/">
             <img src={profile} className="profileImg" />
