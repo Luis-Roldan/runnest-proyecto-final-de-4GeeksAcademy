c1a4e19f1080228dcf240bf3246146c66d553fdd
Luis Fernando Roldan Ortiz <108189366+Luis-Roldan@users.noreply.github.com>
2024-01-25 12:07:47 +0000

    agregado una plantilla para posible coneccion con backend pero falta probar su funcionalidad

--- src/front/js/pages/signUpForOrganizers.jsx
+++ src/front/js/pages/signUpForOrganizers.jsx
@@ -2,32 +2,68 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/signUpForOrganizers.css";
+import axios from "axios";
 
 export const SignUpForOrganizers = () => {
 
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+        nombre: "",
+        apellido: "",
+        telefono: "",
+        direccion: "",
+        terminos: false,
+    });
+
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        setFormData({
+            ...formData,
+            [name]: type === "checkbox" ? checked : value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post(
+                "http://localhost:5000/user",
+                formData
+            );
+            console.log(response.data);
+        } catch (error) {
+            console.error("Error al enviar el formulario", error);
+        }
+    };
 
     return (
         <div className="SignUpForOrganizersContainer">
             <h1 className="TitleSignUpForOrganizers">
                 Registro para organizadores
             </h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="Name" className="form-label">Nombre</label>
-                    <input type="text" className="form-control" id="Name" />
+                    <input type="text" className="form-control" id="Name" onChange={handleChange}
+                        value={formData.nombre} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="lastName" className="form-label">Apellido</label>
-                    <input type="text" className="form-control" id="lastName" />
+                    <input type="text" className="form-control" id="lastName" onChange={handleChange}
+                        value={formData.apellido} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="InputEmail" className="form-label">Correo electrónico</label>
-                    <input type="email" className="form-control" id="InputEmail" aria-describedby="emailHelp" />
+                    <input type="email" className="form-control" id="InputEmail" aria-describedby="emailHelp" onChange={handleChange}
+                        value={formData.email} />
                     <div id="emailHelp" className="form-text">Nostros nunca compartiremos tu correo con nadie</div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="phone" className="form-label">Teléfono</label>
-                    <input type="number" className="form-control" id="phone" />
+                    <input type="number" className="form-control" id="phone" onChange={handleChange}
+                        value={formData.telefono} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="OrganizationName" className="form-label">Nombre de la organización</label>
@@ -39,7 +75,8 @@ export const SignUpForOrganizers = () => {
                 </div>
                 <div className="mb-3">
                     <label htmlFor="InputPassword" className="form-label">Contraseña</label>
-                    <input type="password" className="form-control" id="InputPassword" />
+                    <input type="password" className="form-control" id="InputPassword" onChange={handleChange}
+                        value={formData.password} />
                 </div>
                 <div className="mb-3 form-check CheckBoxContainer">
                     <input type="checkbox" className="form-check-input" id="exampleCheck" />
