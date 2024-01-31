d838a71dae268043d9985aa0e2c45f91dbb0e5aa
Luis Fernando Roldan Ortiz <108189366+Luis-Roldan@users.noreply.github.com>
2024-01-31 20:52:36 +0000

    se coloco los inputs de registro de carrera, falta agregar lo de dia, mes y año para el input de  fecha y pasar la info al backend

--- src/front/js/pages/CareerRegistration.jsx
+++ src/front/js/pages/CareerRegistration.jsx
@@ -14,8 +14,40 @@ export const RegistroDeCarreras = () => {
             </h1>
             <form >
                 <div className="mb-3">
-                    <label htmlFor="Name" className="form-label">Nombre Completo</label>
+                    <label htmlFor="Name" className="form-label">Nombre de la carrera</label>
                     <input type="text" className="form-control" id="Name" onChange />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Name" className="form-label">Distancia</label>
+                    <input type="text" className="form-control" id="Name" onChange />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Name" className="form-label">Ciudad</label>
+                    <input type="text" className="form-control" id="Name" onChange />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Name" className="form-label">País</label>
+                    <input type="text" className="form-control" id="Name" onChange />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Name" className="form-label">Fecha</label>
+                    <input type="text" className="form-control" id="Name" onChange />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Name" className="form-label">Costo</label>
+                    <input type="text" className="form-control" id="Name" onChange />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Name" className="form-label">Dificultad</label>
+                    <input type="text" className="form-control" id="Name" onChange />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Name" className="form-label">Capacidad</label>
+                    <input type="text" className="form-control" id="Name" onChange />
+                </div>
+                <div className="mb-3 form-check CheckBoxContainer">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck" />
+                    <label className="form-check-label" htmlFor="exampleCheck">Acepto términos y condiciones</label>
                 </div>
 
                 <button type="submit" className="btn btn-primary SubmitButtonForOrganizersSignUp">Enviar</button>
