e567398b170cf583aba2beac218e5b6ae9b0d27d
Luis Fernando Roldan Ortiz <108189366+Luis-Roldan@users.noreply.github.com>
2024-02-01 22:22:22 +0000

    se agrego el perfil de usuario y estilos, frontend

--- src/front/js/pages/perfilUsuario.js
+++ src/front/js/pages/perfilUsuario.js
@@ -4,16 +4,16 @@ import "../../styles/perfil.css"
 
 
 export const PerfilUsuario = () => {
-    const [ isClicked, setIsclicked ] = useState("favoritos")
+    const [isClicked, setIsclicked] = useState("favoritos")
     const { store, actions } = useContext(Context)
 
     //ejecutar la funcion getUserData para hacer un GET y obtener datos del usuario
-    useEffect(() => {actions.getUserData();} , [])
-    
+    useEffect(() => { actions.getUserData(); }, [])
+
     //funcion para renderizar en base al boton que se le hace click
     const handleConditionalRendering = () => {
         if (isClicked == "favoritos") {
-            return(
+            return (
                 <div>Favoritos</div>
             )
         } else if (isClicked == "carreras") {
@@ -24,16 +24,27 @@ export const PerfilUsuario = () => {
             return "resultados"
         }
     }
-    
 
-    return(
+
+    return (
         <div>
-            <h1 className="text-center pt-4">{`Bienvenido, ${store.usuario.nombre}`}</h1>
+            <section className="TopProfilePartContainer">
+                <div className="LeftSideProfileTop">
+                    <img src="https://rajueditor.com/wp-content/uploads/2023/09/fotos-de-perfil-aesthetic.jpg" className="ImgProfile"></img>
+                    <h1 className="text-center pt-4">{`Bienvenido, ${store.usuario.nombre} ${store.usuario.apellido} a la meca de las carreras`}</h1>
+                </div>
+                <div className="RightSideProfileTop">
+                    <h2 className="Title">tus datos personales de usuario son:</h2>
+                    <h3 className="text-center pt-4">{`Correo: ${store.usuario.email}`}</h3>
+                    <h3 className="text-center pt-4">{`Telefono: ${store.usuario.telefono}`}</h3>
+                    <h3 className="text-center pt-4">{`Direccion: ${store.usuario.direccion}`}</h3>
+                </div>
+            </section>
             <div className="d-flex justify-content-center pt-4">
-                <button onClick={()=> {setIsclicked("favoritos")}} className="btn-perfil">Favoritos</button>
-                <button onClick={()=> {setIsclicked("carreras")}} className="btn-perfil">Mis carreras</button>
-                <button onClick={()=> {setIsclicked("reviews")}} className="btn-perfil">Reseñas</button>
-                <button onClick={()=> {setIsclicked("resultados")}} className="btn-perfil">Resultados</button>
+                <button onClick={() => { setIsclicked("favoritos") }} className="btn-perfil">Favoritos</button>
+                <button onClick={() => { setIsclicked("carreras") }} className="btn-perfil">Mis carreras</button>
+                <button onClick={() => { setIsclicked("reviews") }} className="btn-perfil">Reseñas</button>
+                <button onClick={() => { setIsclicked("resultados") }} className="btn-perfil">Resultados</button>
             </div>
             <div className="d-flex justify-content-center pt-4">
                 {handleConditionalRendering()}
