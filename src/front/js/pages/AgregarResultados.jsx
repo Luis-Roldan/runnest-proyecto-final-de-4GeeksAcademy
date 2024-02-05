4aad26161dc83855d68082d011830f8b655ce2e0
Luis Fernando Roldan Ortiz <108189366+Luis-Roldan@users.noreply.github.com>
2024-02-05 20:56:08 +0000

    se agrego la tabla en la pagina agregar resultados para agregar los resultados de los participantes, pero falta poner los inputs

--- src/front/js/pages/AgregarResultados.jsx
+++ src/front/js/pages/AgregarResultados.jsx
@@ -7,6 +7,37 @@ export const AgregarResultados = () => {
 
     return (
         <div>
-            <h1>hollaaaaaaa</h1>
+            <h1>Nombre de la carrera</h1>
+            <h2>Distancia</h2>
+            <h2>Lugar</h2>
+            <table className="table ">
+                <thead>
+                    <tr className="table-dark">
+                        <th scope="col">#</th>
+                        <th scope="col">Participante</th>
+                        <th scope="col">Edad</th>
+                        <th scope="col">Tiempo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>22</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Otto</td>
+                        <td>@fat</td>
+                    </tr>
+                    {/* <tr>
+                        <th scope="row">3</th>
+                        <td colSpan="4">Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr> */}
+                </tbody>
+            </table>
         </div>)
 }
