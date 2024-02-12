8637d0bebabd58ffb63adb96d512a3bca540635c
Luis Fernando Roldan Ortiz <108189366+Luis-Roldan@users.noreply.github.com>
2024-02-12 23:14:52 +0000

    se agrego los tres puestos para subirlos en agregar resultados

--- src/front/js/pages/AgregarResultados.jsx
+++ src/front/js/pages/AgregarResultados.jsx
@@ -4,80 +4,64 @@ import { Context } from "../store/appContext";
 import "../../styles/AgregarResultados.css";
 
 export const AgregarResultados = () => {
-
-
-
     const { id } = useParams();
     const { store, actions } = useContext(Context);
     const carrera = store.carreras.find(carrera => carrera.id === parseInt(id));
 
-    console.log(carrera)
-
-    console.log(store.carreras)
-
-    ///////////////
-
-    const [participante, setParticipante] = useState("")
-    const [edad, setEdad] = useState("")
-    const [horas, setHoras] = useState("")
-    const [minutos, setMinutos] = useState("")
-    const [segundos, setSegundos] = useState("")
-    const [puesto, setPuesto] = useState("")
-
-
-
-    const data = {
-        participante: participante,
-        edad: edad,
-        horas: horas,
-        minutos: minutos,
-        segundos: segundos,
-        carrera_id: id,
-        puesto: puesto
-    }
-
-
-
-
-    useEffect(() => {
-        actions.getCarreras();
-    }, []);
+    const [resultados, setResultados] = useState([
+        { participante: "", edad: "", horas: "", minutos: "", segundos: "", puesto: "1" },
+        { participante: "", edad: "", horas: "", minutos: "", segundos: "", puesto: "2" },
+        { participante: "", edad: "", horas: "", minutos: "", segundos: "", puesto: "3" }
+    ]);
 
     const url = process.env.REACT_ENV_URL;
     const token = localStorage.getItem("accessToken");
 
+    const handleSubmit = () => {
+        try {
+            resultados.forEach(async (result) => {
+                const { participante, edad, horas, minutos, segundos, puesto } = result;
+                const data = {
+                    participante,
+                    edad,
+                    horas,
+                    minutos,
+                    segundos,
+                    carrera_id: id,
+                    puesto
+                };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+                const resp = await fetch(url + "/resultados", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": "Bearer " + token,
+                    },
+                    body: JSON.stringify(data),
+                });
 
-        fetch(url + "/resultados", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + token,
-            },
-            body: JSON.stringify(data),
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
+                if (!resp.ok) {
+                    throw new Error("There was a problem submitting the results");
                 }
-                return response.json();
-            })
-            .then((data) => {
-                console.log(data);
-                setParticipante("");
-                setEdad("");
-                setHoras("");
-                setMinutos("");
-                setSegundos("");
-                setPuesto("");
 
-            })
-            .catch((error) => {
-                console.error("Error submitting form:", error);
+                console.log("Result submitted successfully");
             });
 
+            // Limpiar los campos después de enviar la solicitud
+            setResultados([
+                { participante: "", edad: "", horas: "", minutos: "", segundos: "", puesto: "1" },
+                { participante: "", edad: "", horas: "", minutos: "", segundos: "", puesto: "2" },
+                { participante: "", edad: "", horas: "", minutos: "", segundos: "", puesto: "3" }
+            ]);
+        } catch (error) {
+            console.error("Error submitting results:", error.message);
+        }
+    };
+
+    const handleChange = (index, field, value) => {
+        const updatedResultados = [...resultados];
+        updatedResultados[index][field] = value;
+        setResultados(updatedResultados);
     };
 
     return (
@@ -98,39 +82,45 @@ export const AgregarResultados = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td className="Puesto">
-                                <input type="number" placeholder="puesto" min="1" max="3" className="inputEdad" onChange={(e) => setPuesto(e.target.value)}
-                                    value={puesto} />
-                            </td>
-                            <td className="Participante">
-                                <input type="text" placeholder="nombre" className=" inputParticipante" onChange={(e) => setParticipante(e.target.value)}
-                                    value={participante}>
-                                </input>
-                            </td>
-                            <td className="Edad">
-                                <input type="number" placeholder="edad" min="0" max="120" className="inputEdad" onChange={(e) => setEdad(e.target.value)}
-                                    value={edad} />
-                            </td>
-                            <td className="Time">
-                                <input type="number" placeholder="Horas" min="0" max="60" className="TimeInput" onChange={(e) => setHoras(e.target.value)}
-                                    value={horas} />
-                                <span>:</span>
-                                <input type="number" placeholder="Minutos" min="0" max="60" className="TimeInput" onChange={(e) => setMinutos(e.target.value)}
-                                    value={minutos} />
-                                <span>:</span>
-                                <input type="number" placeholder="Segundos" min="0" max="60" className="TimeInput" onChange={(e) => setSegundos(e.target.value)}
-                                    value={segundos} />
-                            </td>
-                        </tr>
-
+                        {resultados.map((result, index) => (
+                            <tr key={index}>
+                                <td className="Puesto">
+                                    <input type="number" placeholder="puesto" min="1" max="3" className="inputEdad"
+                                        value={result.puesto} readOnly />
+                                </td>
+                                <td className="Participante">
+                                    <input type="text" placeholder="nombre" className=" inputParticipante"
+                                        value={result.participante}
+                                        onChange={(e) => handleChange(index, "participante", e.target.value)} />
+                                </td>
+                                <td className="Edad">
+                                    <input type="number" placeholder="edad" min="0" max="120" className="inputEdad"
+                                        value={result.edad}
+                                        onChange={(e) => handleChange(index, "edad", e.target.value)} />
+                                </td>
+                                <td className="Time">
+                                    <input type="number" placeholder="Horas" min="0" max="60" className="TimeInput"
+                                        value={result.horas}
+                                        onChange={(e) => handleChange(index, "horas", e.target.value)} />
+                                    <span>:</span>
+                                    <input type="number" placeholder="Minutos" min="0" max="60" className="TimeInput"
+                                        value={result.minutos}
+                                        onChange={(e) => handleChange(index, "minutos", e.target.value)} />
+                                    <span>:</span>
+                                    <input type="number" placeholder="Segundos" min="0" max="60" className="TimeInput"
+                                        value={result.segundos}
+                                        onChange={(e) => handleChange(index, "segundos", e.target.value)} />
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </form>
+            {/* Botón para enviar los resultados */}
             <button type="submit" className="btn btn-primary SubmitButtonForCareerRegistration" onClick={handleSubmit}>Enviar</button>
-        </div>)
-}
-
+        </div>
+    );
+};
 
 
 /******************************* */
--- src/front/js/pages/Pruebas.jsx
+++ src/front/js/pages/Pruebas.jsx
@@ -166,3 +166,138 @@ export const Prueba = () => {
             <button type="submit" className="btn btn-primary SubmitButtonForCareerRegistration" onClick={handleSubmit}>Enviar</button>
         </div>)
 }
+
+
+
+//////////////////////////////////
+
+
+
+
+// export const AgregarResultados = () => {
+
+
+
+//     const { id } = useParams();
+//     const { store, actions } = useContext(Context);
+//     const carrera = store.carreras.find(carrera => carrera.id === parseInt(id));
+
+//     console.log(carrera)
+
+//     console.log(store.carreras)
+
+//     ///////////////
+
+//     const [participante, setParticipante] = useState("")
+//     const [edad, setEdad] = useState("")
+//     const [horas, setHoras] = useState("")
+//     const [minutos, setMinutos] = useState("")
+//     const [segundos, setSegundos] = useState("")
+//     const [puesto, setPuesto] = useState("")
+
+
+
+//     const data = {
+//         participante: participante,
+//         edad: edad,
+//         horas: horas,
+//         minutos: minutos,
+//         segundos: segundos,
+//         carrera_id: id,
+//         puesto: puesto
+//     }
+
+
+
+
+//     useEffect(() => {
+//         actions.getCarreras();
+//     }, []);
+
+//     const url = process.env.REACT_ENV_URL;
+//     const token = localStorage.getItem("accessToken");
+
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+
+//         fetch(url + "/resultados", {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "Authorization": "Bearer " + token,
+//             },
+//             body: JSON.stringify(data),
+//         })
+//             .then((response) => {
+//                 if (!response.ok) {
+//                     throw new Error(`HTTP error! Status: ${response.status}`);
+//                 }
+//                 return response.json();
+//             })
+//             .then((data) => {
+//                 console.log(data);
+//                 setParticipante("");
+//                 setEdad("");
+//                 setHoras("");
+//                 setMinutos("");
+//                 setSegundos("");
+//                 setPuesto("");
+
+//             })
+//             .catch((error) => {
+//                 console.error("Error submitting form:", error);
+//             });
+
+//     };
+
+//     return (
+//         <div>
+//             <div className="DatosDeLaCarrera">
+//                 <h1 className="TituloPrincipal">{`Nombre de la carrera: ${carrera?.nombre}`}</h1>
+//                 <h2 className="Titulo">{`Distancia: ${carrera?.distancia}`}</h2>
+//                 <h2 className="Titulo">{`Lugar: ${carrera?.ciudad}`}</h2>
+//             </div>
+//             <form>
+//                 <table className="table ">
+//                     <thead>
+//                         <tr className="table-dark">
+//                             <th scope="col">Puesto</th>
+//                             <th scope="col">Participante</th>
+//                             <th scope="col">Edad</th>
+//                             <th scope="col">Tiempo</th>
+//                         </tr>
+//                     </thead>
+//                     <tbody>
+//                         <tr>
+//                             <td className="Puesto">
+//                                 <input type="number" placeholder="puesto" min="1" max="3" className="inputEdad" onChange={(e) => setPuesto(e.target.value)}
+//                                     value={puesto} />
+//                             </td>
+//                             <td className="Participante">
+//                                 <input type="text" placeholder="nombre" className=" inputParticipante" onChange={(e) => setParticipante(e.target.value)}
+//                                     value={participante}>
+//                                 </input>
+//                             </td>
+//                             <td className="Edad">
+//                                 <input type="number" placeholder="edad" min="0" max="120" className="inputEdad" onChange={(e) => setEdad(e.target.value)}
+//                                     value={edad} />
+//                             </td>
+//                             <td className="Time">
+//                                 <input type="number" placeholder="Horas" min="0" max="60" className="TimeInput" onChange={(e) => setHoras(e.target.value)}
+//                                     value={horas} />
+//                                 <span>:</span>
+//                                 <input type="number" placeholder="Minutos" min="0" max="60" className="TimeInput" onChange={(e) => setMinutos(e.target.value)}
+//                                     value={minutos} />
+//                                 <span>:</span>
+//                                 <input type="number" placeholder="Segundos" min="0" max="60" className="TimeInput" onChange={(e) => setSegundos(e.target.value)}
+//                                     value={segundos} />
+//                             </td>
+//                         </tr>
+
+//                     </tbody>
+//                 </table>
+//             </form>
+//             <button type="submit" className="btn btn-primary SubmitButtonForCareerRegistration" onClick={handleSubmit}>Enviar</button>
+//         </div>)
+// }
