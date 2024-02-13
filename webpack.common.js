ae369d0b58d6b66cc375e523bb0b4722430634e9
Sofia Vanessa <105688383+sofiat25@users.noreply.github.com>
2024-02-13 02:54:11 +0000

    cambios en el icono y el titulo de la pagina

--- webpack.common.js
+++ webpack.common.js
@@ -40,7 +40,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        favicon: '4geeks.icon',
+        favicon: 'runestLogo_2.ico',
         template: 'template.html'
     }),
     new Dotenv({ safe: true, systemvars: true })
